688b1116db86ab332cefeb0b4d9acf1b44fb93e4
Jeff Akins <jeffakins21@gmail.com>
2021-08-13 11:54:25 -0700

    Data cleaning using the Iris Data Set

--- acquire.py
+++ acquire.py
@@ -1,26 +1,41 @@
 # Data Acquisition Exercises
 
+#---------------------------Imports---------------------------------------------------
+
 import env
 import pandas as pd
 import numpy as np
 import os
 
-#-----------------------------------------------------------------------------------
+#---------------------------Connection Info--------------------------------------------
 
 # Connection information for the mySQL Server
 
 def get_connection(db, user=env.username, host=env.hostname, password=env.password):
-    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+    connection_info = f'mysql+pymysql://{user}:{password}@{host}/{db}'
+    return connection_info
 
-#-----------------------------------------------------------------------------------
+#---------------------------Data Base Functions----------------------------------------
 
-# Titanic Data Set from CODEUP's mySQL Server 
+# Function to retrieve the Titanic Data Set from CODEUP's mySQL Server 
 def get_titanic_data():
-    sql = 'SELECT * FROM passengers;'
-    db = 'titanic_db'
-    df = pd.read_sql(sql, get_connection(db))
+    if os.path.isfile('titanic_df.csv'):
+        df = pd.read_csv('titanic_df.csv', index_col=0) # If csv file exists read in data from csv file.
+    else:
+        sql = 'SELECT * FROM passengers;'               # SQL query
+        db = 'titanic_db'                               # Database name
+        df = pd.read_sql(sql, get_connection(db))       # Pandas DataFrame
+        df.to_csv('titanic_df.csv')                     # Cache Data
     return df
 
- # Iris Dataset   
-#def get_iris_data():
-#    return 
+# Function to retrieve the Iris Dataset   
+def get_iris_data():
+    if os.path.isfile('iris_df.csv'):
+        df = pd.read_csv('iris_df.csv', index_col=0)    # If csv file exists read in data from csv file.
+    else:
+        sql = 'SELECT * FROM measurements JOIN species USING(species_id);'
+        db = 'iris_db'
+        df = pd.read_sql(sql, get_connection(db))       # Read fresh data from db into a DataFrame
+        df.to_csv('iris_df.csv')                        # Cache data 
+    return df
+
